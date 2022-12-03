1d90baaf816bde5e1a0ae8642f3d6ee8f24a4e51
007anthony <anthony.pena@lernende.bbw.ch>
2022-12-03 15:06:42 +0100

    added questions again to Quiz

--- src/main/java/ch/bbw/ap/quizbackend/model/Quiz.java
+++ src/main/java/ch/bbw/ap/quizbackend/model/Quiz.java
@@ -3,21 +3,24 @@ package ch.bbw.ap.quizbackend.model;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Quiz {
 
     private String name;
     private User createdBy;
     private LocalDate creationDate;
+    private List<Question> questions;
 
     public Quiz() {
-        this(null, null, null);
+        this(null, null, null, null);
     }
 
-    public Quiz(String name, User createdBy, LocalDate creationDate) {
+    public Quiz(String name, User createdBy, LocalDate creationDate, List<Question> questions) {
         this.name = name;
         this.createdBy = createdBy;
         this.creationDate = creationDate;
+        this.questions = questions;
     }
 
 
@@ -43,5 +46,13 @@ public class Quiz {
 
     public void setCreationDate(LocalDate creationDate) {
         this.creationDate = creationDate;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
     }
 }
