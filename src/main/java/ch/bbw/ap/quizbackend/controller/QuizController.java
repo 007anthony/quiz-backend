92ae4d80b95cce83e23739b9a69ed8f1c03fcae6
007anthony <anthony.pena@lernende.bbw.ch>
2022-12-03 19:17:10 +0100

    created first controller method

---
+++ src/main/java/ch/bbw/ap/quizbackend/controller/QuizController.java
@@ -0,0 +1,22 @@
+package ch.bbw.ap.quizbackend.controller;
+
+import ch.bbw.ap.quizbackend.model.Quiz;
+import ch.bbw.ap.quizbackend.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @GetMapping("/quiz/all")
+    public List<Quiz> allQuizes() {
+        return quizService.getQuizes();
+    }
+}
